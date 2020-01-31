3bd114ee474a9fea958c9fc8e542f7f48c6de245
RodwelJunior09 <rodweljunior@gmail.com>
2020-01-30 23:19:37 -0400

    Space Wars Paddle Gameplay

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Configuration Variables
+    [SerializeField] float screenWidthUnits = 16;
+    [SerializeField] float minScreenSize;
+    [SerializeField] float maxScreenSize;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var xMouse = Input.mousePosition.x / Screen.width * screenWidthUnits;
+        Vector2 movement = new Vector2(transform.position.x, transform.position.y);
+        movement.x = Mathf.Clamp(xMouse, minScreenSize, maxScreenSize);
+        transform.position = movement;
+    }
+}
---
+++ Assets/Scripts/SceneFlow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneFlow : MonoBehaviour
+{
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadNextScene()
+    {
+        int currentIndexScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndexScene + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
